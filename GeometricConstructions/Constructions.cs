d60952c25c51a71c08b1dad21d887ac5378deabf
Mason Boeman <maboesanman@gmail.com>
2016-08-16 13:07:05 -0500

    added many new functions including common triangle centers, parallel and perpendicular lines, and angle/perpendicular bisectors.

--- GeometricConstructions/Constructions.cs
+++ GeometricConstructions/Constructions.cs
@@ -39,16 +39,16 @@ namespace System.Windows
 
             double discriminant = c.Radius * c.Radius * dr * dr - D * D;
 
-            if(discriminant<0)
+            if (discriminant < 0)
             {
                 return new List<Point>();
-            } else if(discriminant==0)
+            } else if (discriminant == 0)
             {
                 double x = D * d.Y / (dr * dr) + c.Center.X;
                 double y = -D * d.X / (dr * dr) + c.Center.Y;
 
-                return new List<Point> { new Point(x,y)};
-            } else 
+                return new List<Point> { new Point(x, y) };
+            } else
             {
                 double x = D * d.Y / (dr * dr) + c.Center.X;
                 double y = -D * d.X / (dr * dr) + c.Center.Y;
@@ -82,7 +82,7 @@ namespace System.Windows
 
             // x is the position along the line which passes through the centers of the circles
             double x = (d * d + r1 * r1 - r2 * r2) / (2 * d);
-            
+
             // and p1 is the (shared) projection of the intersections to the line through the centers
             Point p1 = c1.Center + (c2.Center - c1.Center) * x / d;
 
@@ -96,5 +96,145 @@ namespace System.Windows
             // finally return the intersections with one of the circles and the line throug the computed points.
             return Intersect(c1, new Line(p1, p2));
         }
+        public static Point Project(Point p, Line l)
+        {
+            //project a point onto a line
+            Vector v1 = p - l.Point1;
+            Vector v2 = l.Point2 - l.Point1;
+            return l.Point1 + v2 * (v1 * v2) / v2.LengthSquared;
+        }
+        public static Line Perpendicular(Line l, Point p)
+        {
+            // produce a line perpendicular to l through p
+
+            Vector v = l.Point2 - l.Point1;
+            v = new Vector(-v.Y, v.X); //rotate v 90 degrees
+            Point q = p + v;
+            return new Line(p, q);
+        }
+        public static Line Parallel(Line l, Point p)
+        {
+            // produce a line parallel to l through p
+            return new Line(p, p + (l.Point2 - l.Point1));
+        }
+        public static Line PerpendicularBisector(Point p1, Point p2)
+        {
+            // produce the perpendicular bisector of the segment connecting p1 and p2
+            return Perpendicular(new Line(p1, p2), Midpoint(p1, p2));
+        }
+        public static List<Line> AngleBisector(Line l1, Line l2)
+        {
+            List<Point> c = Intersect(l1, l2);
+            if (c.Count == 0) //if they are parallel, return the line of points equidistant from the two lines.
+                return new List<Line> { new Line(Midpoint(l1.Point1, l2.Point1), Midpoint(l1.Point1, l2.Point2)) };
+            Point center = c[0];
+            Vector v1 = l1.Point2 - l1.Point1;
+            Vector v2 = l2.Point2 - l2.Point1;
+            v1.Normalize();
+            v2.Normalize();
+            List<Line> result = new List<Line>();
+            result.Add(new Line(center, center + v1 + v2));
+            result.Add(new Line(center, center + v1 - v2));
+            return result;
+        }
+
+        private static Point Barycentric(Point A, Point B, Point C, double a, double b, double c)
+        {
+            Point o = new Point(0, 0);
+            Vector vA = A - o;
+            Vector vB = B - o;
+            Vector vC = C - o;
+            return o + (vA * a + vB * b + vC * c) / (a + b + c);
+        }
+
+        public static List<Point> Incenter(Point p1, Point p2, Point p3)
+        {
+            // this returns a list containing the incenter, followed by the excenters opposite p1, p2, and p3 respectively.
+            // Barycentric coordinates from http://mathworld.wolfram.com/BarycentricCoordinates.html
+            double d1 = (p2 - p3).Length;
+            double d2 = (p1 - p3).Length;
+            double d3 = (p1 - p2).Length;
+            List<Point> result = new List<Point>();
+            result.Add(Barycentric(p1, p2, p3, d1, d2, d3));
+            result.Add(Barycentric(p1, p2, p3, -d1, d2, d3));
+            result.Add(Barycentric(p1, p2, p3, d1, -d2, d3));
+            result.Add(Barycentric(p1, p2, p3, d1, d2, -d3));
+
+            return result;
+        }
+
+        public static List<Point> Incenter(Line l1, Line l2, Line l3)
+        {
+            // for a triangle defined by its sides
+            Point p1 = Intersect(l2, l3)[0];
+            Point p2 = Intersect(l1, l3)[0];
+            Point p3 = Intersect(l1, l2)[0];
+            return Incenter(p1, p2, p3);
+        }
+
+        public static Point Centroid(Point p1, Point p2, Point p3)
+        {
+            // Barycentric coordinates from http://mathworld.wolfram.com/BarycentricCoordinates.html
+            return Barycentric(p1, p2, p3, 1, 1, 1);
+        }
+
+        public static Point Centroid(Line l1, Line l2, Line l3)
+        {
+            // for a triangle defined by its sides
+            Point p1 = Intersect(l2, l3)[0];
+            Point p2 = Intersect(l1, l3)[0];
+            Point p3 = Intersect(l1, l2)[0];
+            return Centroid(p1, p2, p3);
+        }
+
+        public static Point Circumcenter(Point p1, Point p2, Point p3)
+        {
+            // Barycentric coordinates from http://mathworld.wolfram.com/BarycentricCoordinates.html
+
+            double d1 = (p2 - p3).Length;
+            double d2 = (p1 - p3).Length;
+            double d3 = (p1 - p2).Length;
+
+            double w1 = d1 * d1 * (d2 * d2 + d3 * d3 - d1 * d1);
+            double w2 = d2 * d2 * (d3 * d3 + d1 * d1 - d2 * d2);
+            double w3 = d3 * d3 * (d1 * d1 + d2 * d2 - d3 * d3);
+
+            return Barycentric(p1, p2, p3, w1, w2, w3);
+        }
+
+        public static Point Circumcenter(Line l1, Line l2, Line l3)
+        {
+            // for a triangle defined by its sides
+            Point p1 = Intersect(l2, l3)[0];
+            Point p2 = Intersect(l1, l3)[0];
+            Point p3 = Intersect(l1, l2)[0];
+            return Circumcenter(p1, p2, p3);
+        }
+
+        public static Point Orthocenter(Point p1, Point p2, Point p3)
+        {
+            // Barycentric coordinates from http://mathworld.wolfram.com/BarycentricCoordinates.html
+
+            double d1 = (p2 - p3).Length;
+            double d2 = (p1 - p3).Length;
+            double d3 = (p1 - p2).Length;
+
+            double m1 = (d2 * d2 + d3 * d3 - d1 * d1);
+            double m2 = (d3 * d3 + d1 * d1 - d2 * d2);
+            double m3 = (d1 * d1 + d2 * d2 - d3 * d3);
+
+            return Barycentric(p1, p2, p3, m2 * m3, m1 * m3, m1 * m2);
+        }
+
+        public static Point Orthocenter(Line l1, Line l2, Line l3)
+        {
+            // for a triangle defined by its sides
+            Point p1 = Intersect(l2, l3)[0];
+            Point p2 = Intersect(l1, l3)[0];
+            Point p3 = Intersect(l1, l2)[0];
+            return Orthocenter(p1, p2, p3);
+        }
+
+
     }
 }
--- GeometricConstructionsTest/ConstructionsTest.cs
+++ GeometricConstructionsTest/ConstructionsTest.cs
@@ -84,8 +84,8 @@ namespace GeometricConstructionsTest
             List<Point> Intersections = Constructions.Intersect(l, c);
 
             // assert
-            Assert.AreEqual(0, Intersections[0].X, .00000001); // test the x and y coordinates separately and use a tolerance
-            Assert.AreEqual(2, Intersections[0].Y, .00000001);
+            Assert.AreEqual(0, Intersections[0].X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(2, Intersections[0].Y, .00000001, "Y is out of range");
 
         }
         [TestMethod]
@@ -99,8 +99,8 @@ namespace GeometricConstructionsTest
             List<Point> Intersections = Constructions.Intersect(l, c);
 
             // assert
-            Assert.AreEqual(2, Intersections[1].X, .00000001); // test the x and y coordinates separately and use a tolerance
-            Assert.AreEqual(0, Intersections[1].Y, .00000001);
+            Assert.AreEqual(2, Intersections[1].X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(0, Intersections[1].Y, .00000001, "Y is out of range");
         }
         [TestMethod]
         public void CircleCircleIntersectTest_1()
@@ -113,8 +113,8 @@ namespace GeometricConstructionsTest
             List<Point> Intersections = Constructions.Intersect(c1, c2);
 
             // assert
-            Assert.AreEqual(1, Intersections[1].X, .00000001); // test the x and y coordinates separately and use a tolerance
-            Assert.AreEqual(-1, Intersections[1].Y, .00000001);
+            Assert.AreEqual(1, Intersections[1].X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(-1, Intersections[1].Y, .00000001, "Y is out of range");
         }
         [TestMethod]
         public void CircleCircleIntersectTest_2_1()
@@ -127,8 +127,8 @@ namespace GeometricConstructionsTest
             List<Point> Intersections = Constructions.Intersect(c1, c2);
 
             // assert
-            Assert.AreEqual(20, Intersections[0].X, .00000001); // test the x and y coordinates separately and use a tolerance
-            Assert.AreEqual(27, Intersections[0].Y, .00000001);
+            Assert.AreEqual(20, Intersections[0].X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(27, Intersections[0].Y, .00000001, "Y is out of range");
         }
         [TestMethod]
         public void CircleCircleIntersectTest_2_2()
@@ -141,8 +141,240 @@ namespace GeometricConstructionsTest
             List<Point> Intersections = Constructions.Intersect(c1, c2);
 
             // assert
-            Assert.AreEqual(27, Intersections[1].X, .00000001); // test the x and y coordinates separately and use a tolerance
-            Assert.AreEqual(20, Intersections[1].Y, .00000001);
+            Assert.AreEqual(27, Intersections[1].X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(20, Intersections[1].Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void PointToLineProjection_1()
+        {
+            // arrange
+            Point p = new Point(2, 4);
+            Line l = new Line(new Point(-1, 0), new Point(1, 1));
+
+            // act
+            Point q = Constructions.Project(p, l);
+
+            // assert
+            Assert.AreEqual(3, q.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(2, q.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void PointToLineProjection_2()
+        {
+            // arrange
+            Point p = new Point(2, 2);
+            Line l = new Line(new Point(0, 0), new Point(1, 1));
+
+            // act
+            Point q = Constructions.Project(p, l);
+
+            // assert
+            Assert.AreEqual(2, q.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(2, q.Y, .00000001, "Y is out of range");
+        }
+
+        [TestMethod]
+        public void Perpendicular_1()
+        {
+            // arrange
+            Point p = new Point(2, 2);
+            Line l = new Line(new Point(0, 0), new Point(30, 20));
+
+            // act
+            Point q = Constructions.Project(p, l);
+            Point r = Constructions.Intersect(l, Constructions.Perpendicular(l, p))[0];
+
+            // assert
+            Assert.AreEqual(r.X, q.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(r.Y, q.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void Parallel_1()
+        {
+            // arrange
+            Point p = new Point(2, 2);
+            Line l = new Line(new Point(0, 0), new Point(30, 20));
+
+            // act
+            int x = Constructions.Intersect(l, Constructions.Parallel(l, p)).Count;
+
+            // assert
+            Assert.AreEqual(0, x); // parallel lines have no intersection point
+        }
+        [TestMethod]
+        public void PerpendicularBisector_1()
+        {
+            // arrange
+            Point p1 = new Point(2, 2);
+            Point p2 = new Point(3, 2);
+
+            // act
+            Line l = Constructions.PerpendicularBisector(p1, p2);
+            Point q = Constructions.Project(l.Point1, l);
+            Point r = Constructions.Midpoint(p1, p2);
+
+            // assert
+            Assert.AreEqual(r.X, q.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(r.Y, q.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void AngleBisector_1()
+        {
+            // arrange
+            Point p1 = new Point(2, 2);
+            Point p2 = new Point(3, 2);
+            Point p3 = new Point(5, 3);
+            Line l1 = new Line(p3, p1);
+            Line l2 = new Line(p3, p2);
+
+
+            // act
+            Line l = Constructions.AngleBisector(l1,l2)[0];
+            Point q = Constructions.Project(l.Point1, l1);
+            Point r = Constructions.Project(l.Point1, l2);
+
+            // assert
+            Assert.AreEqual((l.Point1-q).Length,(l.Point1-r).Length, .00000001, "point on bisector not equidistant");
+        }
+        [TestMethod]
+        public void Incenter_1()
+        {
+            // arrange
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(3, 0);
+            Point p3 = new Point(3, 4);
+
+            // act
+            List<Point> points = Constructions.Incenter(p1, p2, p3);
+
+            // assert
+            Assert.AreEqual(2, points[0].X, .00000001, "incenter X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(1, points[0].Y, .00000001, "incenter Y is out of range");
+
+            Assert.AreEqual(6, points[1].X, .00000001, "excenter1 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(3, points[1].Y, .00000001, "excenter1 Y is out of range");
+
+            Assert.AreEqual(-3, points[2].X, .00000001, "excenter2 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(6, points[2].Y, .00000001, "excenter2 Y is out of range");
+
+            Assert.AreEqual(1, points[3].X, .00000001, "excenter3 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(-2, points[3].Y, .00000001, "excenter3 Y is out of range");
+        }
+        [TestMethod]
+        public void Incenter_2()
+        {
+            // arrange (this is the same as the previous test, but with lines instead of points. the triangle is identical.
+            Line l1 = new Line(new Point(3, -1), new Point(3, 1));
+            Line l2 = new Line(new Point(-3, -4), new Point(6, 8));
+            Line l3 = new Line(new Point(1, 0), new Point(2, 0));
+
+            // act
+            List<Point> points = Constructions.Incenter(l1, l2, l3);
+
+            // assert
+            Assert.AreEqual(2, points[0].X, .00000001, "incenter X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(1, points[0].Y, .00000001, "incenter Y is out of range");
+
+            Assert.AreEqual(6, points[1].X, .00000001, "excenter1 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(3, points[1].Y, .00000001, "excenter1 Y is out of range");
+
+            Assert.AreEqual(-3, points[2].X, .00000001, "excenter2 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(6, points[2].Y, .00000001, "excenter2 Y is out of range");
+
+            Assert.AreEqual(1, points[3].X, .00000001, "excenter3 X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(-2, points[3].Y, .00000001, "excenter3 Y is out of range");
+        }
+        [TestMethod]
+        public void Circumcenter_1()
+        {
+            // arrange
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(3, 0);
+            Point p3 = new Point(3, 4);
+
+            // act
+            Point p = Constructions.Circumcenter(p1, p2, p3);
+
+            // assert
+            Assert.AreEqual(1.5, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(2, p.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void Circumcenter_2()
+        {
+            // arrange (this is the same as the previous test, but with lines instead of points. the triangle is identical.
+            Line l1 = new Line(new Point(3, -1), new Point(3, 1));
+            Line l2 = new Line(new Point(-3, -4), new Point(6, 8));
+            Line l3 = new Line(new Point(1, 0), new Point(2, 0));
+
+            // act
+            Point p = Constructions.Circumcenter(l1, l2, l3);
+
+            // assert
+            Assert.AreEqual(1.5, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(2, p.Y, .00000001, "Y is out of range");
+        }
+
+        [TestMethod]
+        public void Orthocenter_1()
+        {
+            // arrange
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(3, 0);
+            Point p3 = new Point(3, 4);
+
+            // act
+            Point p = Constructions.Orthocenter(p1, p2, p3);
+
+            // assert
+            Assert.AreEqual(3, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(0, p.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void Orthocenter_2()
+        {
+            // arrange (this is the same as the previous test, but with lines instead of points. the triangle is identical.
+            Line l1 = new Line(new Point(3, -1), new Point(3, 1));
+            Line l2 = new Line(new Point(-3, -4), new Point(6, 8));
+            Line l3 = new Line(new Point(1, 0), new Point(2, 0));
+
+            // act
+            Point p = Constructions.Orthocenter(l1, l2, l3);
+
+            // assert
+            Assert.AreEqual(3, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(0, p.Y, .00000001, "Y is out of range");
+        }
+
+        [TestMethod]
+        public void Centroid_1()
+        {
+            // arrange
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(3, 0);
+            Point p3 = new Point(3, 4);
+
+            // act
+            Point p = Constructions.Centroid(p1, p2, p3);
+
+            // assert
+            Assert.AreEqual(2, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(4.0/3.0, p.Y, .00000001, "Y is out of range");
+        }
+        [TestMethod]
+        public void Centroid_2()
+        {
+            // arrange (this is the same as the previous test, but with lines instead of points. the triangle is identical.
+            Line l1 = new Line(new Point(3, -1), new Point(3, 1));
+            Line l2 = new Line(new Point(-3, -4), new Point(6, 8));
+            Line l3 = new Line(new Point(1, 0), new Point(2, 0));
+
+            // act
+            Point p = Constructions.Centroid(l1, l2, l3);
+
+            // assert
+            Assert.AreEqual(2, p.X, .00000001, "X is out of range"); // test the x and y coordinates separately and use a tolerance
+            Assert.AreEqual(4.0 / 3.0, p.Y, .00000001, "Y is out of range");
         }
     }
 }
